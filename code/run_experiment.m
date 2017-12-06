e81db897cbce28d7474045ef8e0a30e86fcbf376
Samuel Giacomelli <samuelgiacomelli@gmail.com>
2017-12-06 17:45:06 +0100

    edits on bonus_2 (multiple values of C)

--- code/run_experiment.m
+++ code/run_experiment.m
@@ -11,7 +11,7 @@ function [success_rate, results] = run_experiment(alpha, N, epochs, repetitions,
     end
 
     % run the experiments
-    fprintf('Running experiment for alpha=%.2f, N=%3d ... ', alpha, N);
+    fprintf('Running experiment for alpha=%.2f, N=%3d, epochs=%d, c=%1.2f ... ', alpha, N, epochs, c);
     results = zeros(repetitions, 1);
     parfor i = 1:repetitions
         
